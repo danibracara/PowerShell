898ce00f3a34271600b9b0a08478747e4f36cdb7
Daniel Lopes <43210189+danibracara@users.noreply.github.com>
2024-06-11 21:10:40 -0400

    push

--- test.ps1
+++ test.ps1
@@ -1,2 +1,7 @@
 $prompt=Read-Host "What do you wanna prompt?"
 Write-Host $prompt -ForegroundColor Red
+$confirmation=Read-Host "You Sure? Y / N"
+switch ($confirmation) {
+    Y { Write-Host "Confirmation received" -ForegroundColor Green }
+    N { Write-Host "Denied!" -ForegroundColor Red}
+}

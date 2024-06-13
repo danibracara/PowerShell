d6d7e0bf8b9e975699cbcf02b6424320f508a57e
Daniel Lopes <43210189+danibracara@users.noreply.github.com>
2024-06-12 21:44:56 -0400

    Added complexity to Switch statement

--- test.ps1
+++ test.ps1
@@ -1,7 +1,10 @@
 $prompt=Read-Host "What do you wanna prompt?"
 Write-Host $prompt -ForegroundColor Red
-$confirmation=Read-Host "You Sure? Yes / No"
-switch ($confirmation) {
-    Yes { Write-Host "Confirmation received" -ForegroundColor Green }
-    No { Write-Host "Denied!" -ForegroundColor Red}
-}
+do{
+    $confirmation=Read-Host "You Sure? Yes / No"
+    switch ($confirmation) {
+        Yes { Write-Host "Confirmation received" -ForegroundColor Green }
+        No { Write-Host "Denied!" -ForegroundColor Red}
+        default { Write-Host "You have to type Yes or no"}
+}}while($confirmation -notlike "Yes" || $confirmation -notlike "No")
+
